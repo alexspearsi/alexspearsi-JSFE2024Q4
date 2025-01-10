4c2d90599d08057c8a3cf61ab222cdec83d28107
Alex <strelchenko.aleksandr@yandex.ru>
2025-01-10 17:52:02 +0200

    feat: implement functionality for button Next

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 let buttonStart = document.querySelector('.button-start');
 let buttonNewGame = document.querySelector('.button-new-game');
 let buttonRepeatSequence = document.querySelector('.button-repeat-sequence')
+let buttonNext = document.querySelector('.button-next')
 let counterOfRounds = document.querySelector('.counter-of-rounds')
+let numberOfRound = document.querySelector('.counter-of-rounds span');
 let levelItems = document.querySelectorAll('.header__level-1__list li')
 let keyboardOutput = document.querySelector('.keyboard-output')
-keyboardOutput.innerHTML = '\u200B'
-
-
+    keyboardOutput.innerHTML = '\u200B'
 let audio = document.getElementById('click-sound');
 
-let alphabet = 'ABCDEFJHIGKLMNOPQRSTUVWXYZ';
-let alphabetWithNums = 'ABCDEFJHIGKLMNOPQRSTUVWXYZ0123456789';
+let alphabet = 'JBCGKAZFHYDILMNOPQSTUWERVX';
+let alphabetWithNums = 'J9A2H0F6NQK3G7M4B5D1L8IYZCEXWOSTUV';
 let level = '';
 let round = 1;
 let sequence;
 let outputArray = [];
 let isRepeatSequenceClicked  = false;
 
+buttonNewGame.addEventListener('click', () => {
+  round = 1;
+  outputArray = [];
+  keyboardOutput.innerHTML = '\u200B'
+  isRepeatSequenceClicked = false;
+  numberOfRound.innerHTML = 1;
 
+  buttonStart.style.visibility = 'visible';
+  [buttonNewGame, buttonRepeatSequence, counterOfRounds].forEach(item => {
+    item.classList.toggle('display-on'); 
+  })
+
+  levelItems.forEach(item => {
+    item.addEventListener('click', setLevelOfGame)
+  })
+  
+})
 
 
 function setLevelOfGame(event) {
@@ -66,14 +82,28 @@ buttonStart.addEventListener('click', () => {
   // скрывается кнопка "start"
   buttonStart.style.visibility = 'hidden';
 
-
   runAGame();
 })
 
-
+// Переходим на новый раунд
+function proceedNextRound() {
+  round += 1;
+  outputArray = [];
+  isRepeatSequenceClicked = false;
+  keyboardOutput.innerHTML = '\u200B';
+  numberOfRound.innerHTML = round;
+  runAGame();
+}
 
 //ЗАПУСК ИГРЫ
 function runAGame() {
+  buttonNext.removeEventListener('click', proceedNextRound);
+  // в начале игры показываем кнопку Repeat sequance и скрываем кнопку Next
+  buttonRepeatSequence.style.display = 'block';
+  buttonNext.style.display = 'none';
+  buttonRepeatSequence.classList.remove('disabled');
+  isRepeatSequenceClicked = false;
+
   if (level === 'easy') {
     sequence = Array.from({length: round * 2}, () => Math.floor(Math.random() * 10))
   } else if (level === 'medium') {
@@ -81,7 +111,7 @@ function runAGame() {
   } else if (level === 'hard') {
     sequence = Array.from({length: round * 2}, () => alphabetWithNums[Math.floor(Math.random() * alphabetWithNums.length)])
   }
-  console.log(sequence);
+  console.log(`Round ${round}`, sequence);
   setTimeout(() => showSequance(), 600)
 }
 
@@ -115,7 +145,7 @@ function showSequance() {
 
 
 function timeForInput() {
-  keyboardOutput.style.color = '#32CD32';
+  keyboardOutput.style.color = '';
   let counter = 0;
   const symbolsOfKeyboard = Array.from(document.querySelectorAll('.row'))
                                  .flatMap(row => Array.from(row.children));
@@ -126,11 +156,17 @@ function timeForInput() {
     "hard": symbolsOfKeyboard
   }
 
+  // Удаляем предыдущие обработчики (если они есть)
+  keyboard['easy'].forEach(item => item.removeEventListener('click', clickOnKey));
+  keyboard['medium'].forEach(item => item.removeEventListener('click', clickOnKey));
+  keyboard['hard'].forEach(item => item.removeEventListener('click', clickOnKey));
+  document.removeEventListener('keydown', pressOnKey);
+
 
   buttonRepeatSequence.addEventListener('click', () => {
     if (!isRepeatSequenceClicked) {
-      isRepeatSequenceClicked = true;
       outputArray = [];
+      isRepeatSequenceClicked = true;
       keyboardOutput.innerHTML = '\u200B';
       setTimeout(() => showSequance(), 600);
     }
@@ -146,6 +182,8 @@ function timeForInput() {
     keyboard[level].forEach(item => item.removeEventListener('click', clickOnKey));
     document.removeEventListener('keydown', pressOnKey);
 
+
+
     if (text === "WRONG" && isRepeatSequenceClicked) {
       setTimeout(() => {
         keyboardOutput.innerHTML = finish;
@@ -153,8 +191,19 @@ function timeForInput() {
       }, 0); // ДОБАВИТЬ ЗАДЕРЖКУ 100 СЕК, ЧТОБЫ ПОКАЗЫВАЛИСЬ ВСЕ БУКВЫ
     } else {
       setTimeout(() => {
-        keyboardOutput.innerHTML = text;
-        keyboardOutput.style.color = color;
+        // Конец игры. Выиграл!
+        if (round === 5) {
+          keyboardOutput.innerHTML = finish;
+          buttonNext.style.display = 'none';
+          buttonRepeatSequence.style.display = 'block';
+          buttonRepeatSequence.classList.add('disabled');
+          buttonRepeatSequence.style.pointerEvents = 'none';
+
+          buttonNewGame
+        } else {
+          keyboardOutput.innerHTML = text;
+          keyboardOutput.style.color = color;
+        }
       }, 0); // ДОБАВИТЬ ЗАДЕРЖКУ 100 СЕК, ЧТОБЫ ПОКАЗЫВАЛИСЬ ВСЕ БУКВЫ
     }
   }
@@ -171,16 +220,25 @@ function timeForInput() {
     if (matchingKey) processInput(key);
   }
 
+
   // Общая логика обработки ввода
   function processInput(input) {
     keyboardOutput.textContent += input;
     outputArray.push(input);
-    console.log(outputArray);
 
-    if (sequence[counter] != outputArray[counter]) {
-        showResult('WRONG', 'red', 'Try Again!');
+    // аккуратно с нулями
+    if (String(sequence[counter]) !== String(outputArray[counter])) {
+      keyboardOutput.style.color = 'red'; 
+      showResult('WRONG', 'red', 'Try Again!');
     } else if ((counter + 1) === sequence.length) {
+      keyboardOutput.style.color = '#32CD32';
       showResult('RIGHT', '#32CD32', 'Congratulations! You won!');
+
+      buttonRepeatSequence.style.display = 'none';
+      buttonNext.style.display = 'block';
+      
+      buttonNext.addEventListener('click', proceedNextRound)
+
     } else {
       counter++;
     }
